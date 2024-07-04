5fd233490a17740250bc08e3a60010fae3a668ba
petrelevich <petrelevich@yandex.ru>
2024-07-04 09:41:49 +0300

    L03-gradle2-logging

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,11 @@ import org.gradle.plugins.ide.idea.model.IdeaLanguageLevel
 
 plugins {
     idea
+    id("fr.brouillard.oss.gradle.jgitver")
     id("io.spring.dependency-management")
     id("org.springframework.boot") apply false
+    id("name.remal.sonarlint") apply false
+    id("com.diffplug.spotless") apply false
 }
 
 idea {
@@ -42,6 +45,26 @@ allprojects {
             dependency("com.google.guava:guava:$guava")
         }
     }
+
+    configurations.all {
+        resolutionStrategy {
+            failOnVersionConflict()
+
+            force("javax.servlet:servlet-api:2.5")
+            force("commons-logging:commons-logging:1.1.1")
+            force("commons-lang:commons-lang:2.5")
+            force("org.codehaus.jackson:jackson-core-asl:1.8.8")
+            force("org.codehaus.jackson:jackson-mapper-asl:1.8.8")
+            force("org.sonarsource.analyzer-commons:sonar-analyzer-commons:2.8.0.2699")
+            force("org.sonarsource.analyzer-commons:sonar-xml-parsing:2.8.0.2699")
+            force("org.sonarsource.sslr:sslr-core:1.24.0.633")
+            force("org.sonarsource.analyzer-commons:sonar-analyzer-recognizers:2.8.0.2699")
+            force("com.google.code.findbugs:jsr305:3.0.2")
+            force("commons-io:commons-io:2.15.1")
+            force("com.google.errorprone:error_prone_annotations:2.26.1")
+            force("com.google.j2objc:j2objc-annotations:3.0.0")
+        }
+    }
 }
 
 subprojects {
@@ -54,6 +77,34 @@ subprojects {
     tasks.withType<JavaCompile> {
         options.encoding = "UTF-8"
         options.compilerArgs.addAll(listOf("-Xlint:all,-serial,-processing"))
+    }
+
+    apply<name.remal.gradle_plugins.sonarlint.SonarLintPlugin>()
+    apply<com.diffplug.gradle.spotless.SpotlessPlugin>()
+    configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+        java {
+            palantirJavaFormat("2.38.0")
+        }
+    }
+
+    plugins.apply(fr.brouillard.oss.gradle.plugins.JGitverPlugin::class.java)
+    extensions.configure<fr.brouillard.oss.gradle.plugins.JGitverPluginExtension> {
+        strategy("PATTERN")
+        nonQualifierBranches("main,master")
+        tagVersionPattern("\${v}\${<meta.DIRTY_TEXT}")
+        versionPattern(
+            "\${v}\${<meta.COMMIT_DISTANCE}\${<meta.GIT_SHA1_8}" +
+                    "\${<meta.QUALIFIED_BRANCH_NAME}\${<meta.DIRTY_TEXT}-SNAPSHOT"
+        )
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging.showExceptions = true
+        reports {
+            junitXml.required.set(true)
+            html.required.set(true)
+        }
     }
 }
 
