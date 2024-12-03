f554b2beb2d1dc0b323a04b7eab6ac4d25b755d8
petrelevich <petrelevich@yandex.ru>
2024-12-03 16:59:12 +0300

    L39 kafka

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -82,6 +82,9 @@ include ("L38-webflux:client")
 include ("L38-webflux-chat:client-service")
 include ("L38-webflux-chat:datastore-service")
 
+include ("L39-kafka:consumer")
+include ("L39-kafka:producer")
+
 pluginManagement {
     val jgitver: String by settings
     val dependencyManagement: String by settings
